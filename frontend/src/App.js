f26142b94a38a0faf8b8f8d9552207d18c9aac9d
Son Mac <mactranthienson@gmail.com>
2022-05-01 02:47:45 -0700

    Created equation for 'special' missing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,18 @@ import Notes from './components/Notes';
 
 function App() {
   const startingDate = 1636011960;
+  const nhamnhiDate = new Date('02/05/2022');
+  const today = new Date();
+  const difference = Math.floor(
+    (today.getTime() - nhamnhiDate.getTime()) / (1000 * 3600 * 24)
+  );
   return (
-    <Container>
+    <React.Fragment>
       <Timer startingDate={startingDate} />
       <Events />
       <Notes />
       <Gallery />
-    </Container>
+    </React.Fragment>
   );
 }
 
