901477e72d43a6d40bb94837afc10466d97135cb
Son Mac <mactranthienson@gmail.com>
2022-05-05 07:29:49 -0700

    Configurations for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json({ extended: false }));
 const mainRoutes = require('./routes');
